5b7942a1baab23b412ccce0c9d3291147363d88a
jason <jason.perlmutter@gmail.com>
2025-12-05 22:01:58 -0500

    actually commit?

--- scripts/build_embeddings.py
+++ scripts/build_embeddings.py
@@ -1,3 +1,7 @@
+# calculate embeddings for all passages
+# TODO() - semantic_search might be the better way to go about this
+# https://www.sbert.net/examples/sentence_transformer/applications/semantic-search/README.html
+
 import os
 import pickle
 import pandas as pd
@@ -10,16 +14,18 @@ PROCESSED_DIR = os.path.join(BASE_DIR, "data", "processed")
 INPUT_PASSAGE = os.path.join(PROCESSED_DIR, "passages.csv")
 OUTPUT_EMBED = os.path.join(PROCESSED_DIR, "embeddings.pkl")
 
-# --- Step 1: Load CSV ---
-df = pd.read_csv(INPUT_PASSAGE)  # from Step 2 output
+# from Step 2 output
+df = pd.read_csv(INPUT_PASSAGE)
 
-# --- Step 2: Load embeddings if they exist ---
+# see documentation on pretrained models
+# https://www.sbert.net/docs/sentence_transformer/pretrained_models.html
 if os.path.exists(OUTPUT_EMBED):
     print("Embeddings already present.")
 else:
     print("Generating embeddings locally with all-MiniLM-L6-v2...")
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
     embeddings = model.encode(df["passage"].tolist(), show_progress_bar=True)
+
     with open(OUTPUT_EMBED, "wb") as f:
         pickle.dump(embeddings, f)
 
